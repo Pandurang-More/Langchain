b403de5e471d875e7eacb7101504c19ef615eca3
Pandurang-More <morepandurang264@gmail.com>
2026-09-10 21:55:53 +0530

    prompt engineering

---
+++ 4.Langchain_Prompts/prompts_ui.py
@@ -0,0 +1,16 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+model = ChatOpenAI(model="gpt-4o-mini")
+
+st.header('Research Tool')
+
+user_input=st.text_input('enter your prompt')
+
+if st.button('Summarize'):
+    result= model.invoke(user_input)
+    st.write(result.content)
+
+
